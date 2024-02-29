e3d07ed51f7d6186ead3febeb807e3fcd2595afd
ManuRP99 <manuelrampat@gmail.com>
2024-02-29 03:10:06 +0100

    it derailed

--- betterUI/script_betterUI.js
+++ betterUI/script_betterUI.js
@@ -3,10 +3,12 @@ const ctx = canvas.getContext('2d');
 
 
 canvas.width = (window.innerWidth - 50);
-canvas.height = (window.innerHeight - 70);
+canvas.height = (window.innerHeight - 50);
+canvas.style.background = "#222222";
+canvas.style.margin = "10px";
 
-let ship = "}{x}{";
-let shipCargo = "}[=]{"
+let ship = ["}{x}{"];
+let shipCargo = ["}[=]{"];
 
 let origin = [(Math.floor(canvas.width / 2)), (Math.floor(canvas.height / 2))];
 let x = origin[0];
@@ -16,21 +18,31 @@ let y = origin[1];
 let hitbox = [x, (y-10), 31, 14];
 
 let pickupArea = [0, 0, 100, 100];
-let dropoffArea = [];
+let dropoffArea = [canvas.width - 100, canvas.height - 100, 100, 100];
+
+let topWall = [125, 0, 100, origin[1] + 100];
+let botWall = [canvas.width - 225, canvas.height - (origin[1] + 100), 100, origin[1] + 100];
+
 
 const game = () => {
+
 	ctx.fillStyle = "#00ff00";
 	ctx.strokeStyle = "#ffffff";
 	//ctx.strokeRect(x, y - 10, 31, 14);
 	ctx.strokeRect(hitbox[0], hitbox[1], hitbox[2], hitbox[3]);
-	ctx.fillText(ship, x, y);
+
+	ctx.fillText(ship[0], x, y);
 
 	y = y - 30;
 	hitbox = [x, (y-10), 31, 14];
 
-	ctx.fillText(shipCargo, x, y);
+	ctx.fillText(shipCargo[0], x, y);
+
 	ctx.strokeRect(hitbox[0], hitbox[1], hitbox[2], hitbox[3]);
 	ctx.strokeRect(pickupArea[0], pickupArea[1], pickupArea[2], pickupArea[3]);
+	ctx.strokeRect(dropoffArea[0], dropoffArea[1], dropoffArea[2], dropoffArea[3]);
+	ctx.strokeRect(topWall[0], topWall[1], topWall[2], topWall[3]);
+	ctx.strokeRect(botWall[0], botWall[1], botWall[2], botWall[3]);
 };
 
 game();
---
+++ remake/script_hjkl.js
@@ -0,0 +1,165 @@
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+
+canvas.width = (window.innerWidth - 50);
+canvas.height = (window.innerHeight - 50);
+canvas.style.background = "#222222";
+canvas.style.margin = "10px";
+
+
+let ship = ["}{x}{"];
+let shipCargo = ["}[=]{"];
+let working = false;	// to show shipCargo
+
+let origin = [(Math.floor(canvas.width / 2)), (Math.floor(canvas.height / 2))];
+let x = origin[0];
+let y = origin[1];
+let speedMult = 1;
+let xVel = 0;
+let yVel = 0;
+let deaths = 0;
+
+// left, top, right, bottom
+let hitbox = [x, (y-10), (x + 31), (y + 14)];
+
+let pickupArea = [0, 0, 100, 100];
+let dropoffArea = [canvas.width - 100, canvas.height - 100, 100, 100];
+
+let topWall = [125, 0, 100, origin[1] + 100];
+let botWall = [canvas.width - 225, canvas.height - (origin[1] + 100), 100, origin[1] + 100];
+
+// arrow keys event listeners
+document.body.addEventListener('keydown', (ev) => {
+	if (ev.key == 'k'){
+		yVel = -1;
+		xVel = 0;
+	}
+	if (ev.key == 'j'){
+		yVel = 1;
+		xVel = 0;
+	}
+	if (ev.key == 'h'){
+		xVel = -1;
+		yVel = 0;
+	}
+	if (ev.key == 'l'){
+		xVel = 1;
+		yVel = 0;
+	}
+});
+
+
+
+const staticFrame = () => {
+
+	ctx.fillStyle = "#00ff00";
+	ctx.strokeStyle = "#ffffff";
+	//ctx.strokeRect(x, y - 10, 31, 14);
+	ctx.strokeRect(hitbox[0], hitbox[1], hitbox[2], hitbox[3]);
+
+	ctx.fillText(ship[0], x, y);
+
+	y = y - 30;
+	hitbox = [x, (y-10), 31, 14];
+
+	ctx.fillText(shipCargo[0], x, y);
+
+	ctx.strokeRect(hitbox[0], hitbox[1], hitbox[2], hitbox[3]);
+	ctx.strokeStyle = "#aaffaa";
+	ctx.strokeRect(pickupArea[0], pickupArea[1], pickupArea[2], pickupArea[3]);
+	ctx.strokeRect(dropoffArea[0], dropoffArea[1], dropoffArea[2], dropoffArea[3]);
+	ctx.strokeStyle = "#ffaaaa";
+	ctx.strokeRect(topWall[0], topWall[1], topWall[2], topWall[3]);
+	ctx.strokeRect(botWall[0], botWall[1], botWall[2], botWall[3]);
+};
+
+// the fguuuuuuucuaushfaskjdfhaughacñdgfdhsgasdLAKSJDÑFAŚDJGSf
+let backgroundY = 0;
+const background = () => {
+	for (let i = 0; i < 10; i++) {
+		moveBackground(i);
+	}
+	if (backgroundY > canvas.height + 401) {
+		backgroundY = 0;
+	}
+}
+
+const moveBackground = (pos) => {
+	backgroundY += speedMult / 15;
+	ctx.fillStyle = "#aaaaaa"
+	ctx.fillText("#", pos, backgroundY - (pos * 15));
+	ctx.fillText("#", pos * 70, backgroundY);
+	ctx.fillText("#", pos * 90, backgroundY - (pos * 18));
+	ctx.fillText("#", pos * 50, backgroundY - (pos * 20));
+	ctx.fillText("#", pos * 30, backgroundY - (pos * 19));
+};
+
+//staticFrame();
+
+const resetCanvas = () => {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.strokeStyle = "#aaffaa";
+	ctx.strokeRect(pickupArea[0], pickupArea[1], pickupArea[2], pickupArea[3]);
+	ctx.strokeRect(dropoffArea[0], dropoffArea[1], dropoffArea[2], dropoffArea[3]);
+	ctx.strokeStyle = "#ffaaaa";
+	ctx.strokeRect(topWall[0], topWall[1], topWall[2], topWall[3]);
+	ctx.strokeRect(botWall[0], botWall[1], botWall[2], botWall[3]);
+	working = false;
+};
+
+const collisions = () => {
+	wallCollision();
+	zoneCollision();
+};
+
+const zoneCollision = () => {
+	if ((x + 33) < 100 && (y + 8) < 100) {
+		//pickup();
+		console.log('good');
+		working = true;
+	}
+	if ((x + 31) > dropoffArea[0] && (y + 10) > dropoffArea[1]) {
+		//dropoff();
+		console.log('good');
+		working = true;
+	}
+	
+};
+
+const wallCollision = () => {
+	if (x < (topWall[2] + topWall[0]) && x > (topWall[0] - 31) && (y -10) < topWall[3]) {
+		x = origin[0];
+		y = origin[1];
+	} else if (x < (botWall[0] + 100) && (x + 31) > botWall[0] && (y + 8) > botWall[1]) {
+		x = origin[0];
+		y = origin[1];
+	}
+};
+
+const moveShip = () => {
+	x += xVel;
+	y += yVel;
+};
+
+
+const drawShip = () => {
+	ctx.fillStyle = "#00ff00";
+	if (!working) {
+		ctx.fillText(ship, x, y);
+	} else {
+		ctx.fillText(shipCargo, x, y);
+	}
+};
+
+const gameFrame = () => {
+	resetCanvas();
+	background();
+	moveShip();
+	collisions();
+	drawShip();
+	
+};
+
+setInterval(gameFrame, 2);
